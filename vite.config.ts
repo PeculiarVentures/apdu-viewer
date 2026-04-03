1b5c5c1b216a13f52092893f0a967fba6af6f93c
microshine <microshine@mail.ru>
2026-04-03 18:22:46 +0200

    chore: initialize project with Vite, TypeScript, React, and Biome configuration

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
+
+export default defineConfig({
+  plugins: [react(), svgr()],
+  base: "/apdu-viewer/",
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+});
